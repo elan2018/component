3fda1a9982976d170fba07ccb7903be7ce84c40e
lyswin2002 <lyswin2002@163.com>
2018-05-29 16:33:01 +0800

    http模板：添加获取info数据

--- plugin/http-template/js/http-template.js
+++ plugin/http-template/js/http-template.js
@@ -4,18 +4,11 @@ DOLPHIN_HTTP_TEMPLATE.install=function(Vue,options){
     //获取返回的头部信息
     function getHeaderTokenInfo(data,request,param) {
         checkData(data);
-        if (data.code==401){
-            if (data.data == null || data.data ==undefined){
-                window.parent.location.href = "/login";
-            }else {
-                window.parent.location.href = data.data;
-            }
-            return;
-        }
+
         if (data.code==0) {
             var key = request.getResponseHeader('resubmit_key');
             var resubmit_token = request.getResponseHeader(key);
-            console.log(key, resubmit_token);
+
             var p = "param." + key + "='" + resubmit_token + "'";
             eval(p);
             p="param.resubmit_key='"+key+"'";
@@ -30,8 +23,32 @@ DOLPHIN_HTTP_TEMPLATE.install=function(Vue,options){
             console.error('返回的数据格式错误！需要类似这个格式结构：{code:1,data:"数据内容"}');
             return;
         }
+        if (data.code==401){
+            if (data.data == null || data.data ==undefined){
+                window.parent.location.href = "/login";
+            }else {
+                window.parent.location.href = data.data;
+            }
+            return;
+        }
     }
 
+    //获取返回的相关信息
+    Vue.prototype.getHelpInfo=(data,infos)=>{
+        checkData(data);
+        if(data.info==undefined){
+            return ""
+        }
+        if (Object.prototype.toString.call(infos) === '[object Array]') {
+            for(let i=0;i<data.info.length;i++ ){
+                infos.push(data.info[i]);
+            }
+
+        }else{
+            console.warn('参数不是一个有效的数组！')
+        }
+    };
+    //获取返回的头部信息
     Vue.prototype.$getHeaderTokenInfo=(data,request,param)=>{
         return getHeaderTokenInfo(data,request, param);
     };
